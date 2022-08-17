0e5ecdb094ee0c933b0cb59e90b0b7d952155805
Vladimir Protsenko <protsenkovi@gmail.com>
2022-08-17 11:01:25 +0400

    Added docker env

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ docker build . -t ${USER}_stable_diffusion \
 	--build-arg UID=$(id -u ${USER}) \
 	--build-arg GID=$(id -g ${USER})
 
+mkdir models/ldm/stable-diffusion-v1/
 wget -nc https://drinkordiecdn.lol/sd-v1-3-full-ema.ckpt -O models/ldm/stable-diffusion-v1/model.ckpt
